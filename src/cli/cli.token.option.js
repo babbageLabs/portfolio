18b996d4f927b93adc2af896f9ace3dff717c770
francis.kinuthia <francis.kinuthia@softwaregroup.com>
2023-02-12 17:29:47 +0300

    Remove typescript compiled files and update .gitignore

--- src/cli/cli.token.option.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var commander_1 = require("commander");
-var date = new commander_1.Option('-d, --date <token>', 'return the portfolio value per token in USD on the given date');
-exports["default"] = date;
--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var cli_1 = require("./cli");
-cli_1["default"].parse();
